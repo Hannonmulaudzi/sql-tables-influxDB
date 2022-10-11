9652dd07b450b65444694db6538e93fc6b16557e
Hannon Mulaudzi <hannonmulaudzi@gmail.com>
2022-10-11 09:06:20 +0200

    initial commit

---
+++ env.js
@@ -0,0 +1,92 @@
+let org = `hannonmulaudzi@gmail.com`
+let bucket = `operation`
+
+let writeClient = client.getWriteApi(org, bucket, 'ns')
+
+for (let i = 0; i < 5; i++) {
+  let point = new Point('measurement1')
+    .tag('tagname1', 'tagvalue1')
+    .intField('field1', i)
+
+  void setTimeout(() => {
+    writeClient.writePoint(point)
+  }, i * 1000) // separate points by 1 second
+
+  void setTimeout(() => {
+    writeClient.flush()
+  }, 5000)
+}
+
+let queryClient = client.getQueryApi(org)
+let fluxQuery = `from(bucket: "operation")
+ |> range(start: -10m)
+ |> filter(fn: (r) => r._measurement == "measurement1")`
+
+queryClient.queryRows(fluxQuery, {
+  next: (row, tableMeta) => {
+    const tableObject = tableMeta.toObject(row)
+    console.log(tableObject)
+  },
+  error: (error) => {
+    console.error('\nError', error)
+  },
+  complete: () => {
+    console.log('\nSuccess')
+  },
+})
+
+queryClient = client.getQueryApi(org)
+fluxQuery = `from(bucket: "operation")
+ |> range(start: -10m)
+ |> filter(fn: (r) => r._measurement == "measurement1")
+ |> mean()`
+
+queryClient.queryRows(fluxQuery, {
+  next: (row, tableMeta) => {
+    const tableObject = tableMeta.toObject(row)
+    console.log(tableObject)
+  },
+  error: (error) => {
+    console.error('\nError', error)
+  },
+  complete: () => {
+    console.log('\nSuccess')
+  },
+})
+
+let queryClient = client.getQueryApi(org)
+let fluxQuery = `from(bucket: "operation")
+ |> range(start: -10m)
+ |> filter(fn: (r) => r._measurement == "measurement1")`
+
+queryClient.queryRows(fluxQuery, {
+  next: (row, tableMeta) => {
+    const tableObject = tableMeta.toObject(row)
+    console.log(tableObject)
+  },
+  error: (error) => {
+    console.error('\nError', error)
+  },
+  complete: () => {
+    console.log('\nSuccess')
+  },
+})
+
+queryClient = client.getQueryApi(org)
+fluxQuery = `from(bucket: "operation")
+ |> range(start: -10m)
+ |> filter(fn: (r) => r._measurement == "measurement1")
+ |> mean()`
+
+queryClient.queryRows(fluxQuery, {
+  next: (row, tableMeta) => {
+    const tableObject = tableMeta.toObject(row)
+    console.log(tableObject)
+  },
+  error: (error) => {
+    console.error('\nError', error)
+  },
+  complete: () => {
+    console.log('\nSuccess')
+  },
+})
---
+++ migration/001-create-operational-data-table.sql
@@ -0,0 +1,101 @@
+CREATE TABLE mytable(
+   ID INTEGER NOT NULL PRIMARY KEY 
+  ,Demand_Node_1    NUMERIC(7,1) NOT NULL
+  ,Demand_Node_2    NUMERIC(5,1) NOT NULL
+  ,Demand_Node_3    NUMERIC(5,1) NOT NULL
+  ,Demand_Node_4    NUMERIC(5,1) NOT NULL
+  ,Demand_Node_5    NUMERIC(5,1) NOT NULL
+  ,Demand_Node_6    NUMERIC(5,1) NOT NULL
+  ,Demand_Node_7    NUMERIC(5,1) NOT NULL
+  ,Demand_Node_8    NUMERIC(5,1) NOT NULL
+  ,Demand_Node_9    NUMERIC(5,1) NOT NULL
+  ,Demand_Node_10   NUMERIC(5,1) NOT NULL
+  ,Demand_Node_11   NUMERIC(4,1) NOT NULL
+  ,Demand_Node_12   NUMERIC(5,1) NOT NULL
+  ,Demand_Node_13   NUMERIC(5,1) NOT NULL
+  ,Demand_Node_14   NUMERIC(5,1) NOT NULL
+  ,Demand_Node_15   NUMERIC(5,1) NOT NULL
+  ,Demand_Node_16   NUMERIC(5,1) NOT NULL
+  ,Demand_Node_17   NUMERIC(5,1) NOT NULL
+  ,Demand_Node_18   NUMERIC(4,1) NOT NULL
+  ,Demand_Node_19   NUMERIC(5,1) NOT NULL
+  ,Demand_Node_20   NUMERIC(5,1) NOT NULL
+  ,Demand_Node_21   NUMERIC(5,1) NOT NULL
+  ,Demand_Node_22   NUMERIC(5,1) NOT NULL
+  ,Demand_Node_23   NUMERIC(5,1) NOT NULL
+  ,Demand_Node_24   NUMERIC(5,1) NOT NULL
+  ,Demand_Node_25   NUMERIC(4,1) NOT NULL
+  ,Demand_Node_26   NUMERIC(5,1) NOT NULL
+  ,Demand_Node_27   NUMERIC(4,1) NOT NULL
+  ,Demand_Node_28   NUMERIC(5,1) NOT NULL
+  ,Demand_Node_29   NUMERIC(5,1) NOT NULL
+  ,Demand_Node_30   NUMERIC(5,1) NOT NULL
+  ,Demand_Node_31   NUMERIC(5,1) NOT NULL
+  ,Demand_Node_32   NUMERIC(5,1) NOT NULL
+  ,Flow_Link_1      NUMERIC(6,1) NOT NULL
+  ,Flow_Link_2      NUMERIC(6,1) NOT NULL
+  ,Flow_Link_3      NUMERIC(5,1) NOT NULL
+  ,Flow_Link_4      NUMERIC(5,1) NOT NULL
+  ,Flow_Link_5      NUMERIC(5,1) NOT NULL
+  ,Flow_Link_6      NUMERIC(5,1) NOT NULL
+  ,Flow_Link_7      NUMERIC(5,1) NOT NULL
+  ,Flow_Link_8      NUMERIC(5,1) NOT NULL
+  ,Flow_Link_9      NUMERIC(6,1) NOT NULL
+  ,Flow_Link_10     NUMERIC(7,1) NOT NULL
+  ,Flow_Link_11     NUMERIC(7,1) NOT NULL
+  ,Flow_Link_12     NUMERIC(6,1) NOT NULL
+  ,Flow_Link_13     NUMERIC(6,1) NOT NULL
+  ,Flow_Link_14     NUMERIC(5,1) NOT NULL
+  ,Flow_Link_15     NUMERIC(5,1) NOT NULL
+  ,Flow_Link_16     NUMERIC(6,1) NOT NULL
+  ,Flow_Link_17     NUMERIC(6,1) NOT NULL
+  ,Flow_Link_18     NUMERIC(6,1) NOT NULL
+  ,Flow_Link_19     NUMERIC(6,1) NOT NULL
+  ,Flow_Link_20     NUMERIC(5,1) NOT NULL
+  ,Flow_Link_21     NUMERIC(5,1) NOT NULL
+  ,Flow_Link_22     NUMERIC(5,1) NOT NULL
+  ,Flow_Link_23     NUMERIC(6,1) NOT NULL
+  ,Flow_Link_24     NUMERIC(5,1) NOT NULL
+  ,Flow_Link_25     NUMERIC(4,1) NOT NULL
+  ,Flow_Link_26     NUMERIC(6,1) NOT NULL
+  ,Flow_Link_27     NUMERIC(5,1) NOT NULL
+  ,Flow_Link_28     NUMERIC(5,1) NOT NULL
+  ,Flow_Link_29     NUMERIC(6,1) NOT NULL
+  ,Flow_Link_30     NUMERIC(6,1) NOT NULL
+  ,Flow_Link_31     NUMERIC(6,1) NOT NULL
+  ,Flow_Link_32     NUMERIC(6,1) NOT NULL
+  ,Flow_Link_33     NUMERIC(6,1) NOT NULL
+  ,Flow_Link_34     NUMERIC(6,1) NOT NULL
+  ,Pressure_Node_1  BIT  NOT NULL
+  ,Pressure_Node_2  NUMERIC(6,3) NOT NULL
+  ,Pressure_Node_3  NUMERIC(6,3) NOT NULL
+  ,Pressure_Node_4  NUMERIC(6,3) NOT NULL
+  ,Pressure_Node_5  NUMERIC(6,3) NOT NULL
+  ,Pressure_Node_6  NUMERIC(6,3) NOT NULL
+  ,Pressure_Node_7  NUMERIC(6,3) NOT NULL
+  ,Pressure_Node_8  NUMERIC(6,3) NOT NULL
+  ,Pressure_Node_9  NUMERIC(6,3) NOT NULL
+  ,Pressure_Node_10 NUMERIC(6,3) NOT NULL
+  ,Pressure_Node_11 NUMERIC(6,3) NOT NULL
+  ,Pressure_Node_12 NUMERIC(6,3) NOT NULL
+  ,Pressure_Node_13 NUMERIC(6,3)
+  ,Pressure_Node_14 NUMERIC(6,3)
+  ,Pressure_Node_15 NUMERIC(6,3)
+  ,Pressure_Node_16 NUMERIC(6,3)
+  ,Pressure_Node_17 NUMERIC(6,3)
+  ,Pressure_Node_18 NUMERIC(6,3)
+  ,Pressure_Node_19 NUMERIC(6,3)
+  ,Pressure_Node_20 NUMERIC(6,3)
+  ,Pressure_Node_21 NUMERIC(6,3)
+  ,Pressure_Node_22 NUMERIC(6,3)
+  ,Pressure_Node_23 NUMERIC(6,3)
+  ,Pressure_Node_24 NUMERIC(6,3)
+  ,Pressure_Node_25 NUMERIC(6,3)
+  ,Pressure_Node_26 NUMERIC(6,3)
+  ,Pressure_Node_27 NUMERIC(6,3)
+  ,Pressure_Node_28 NUMERIC(6,3)
+  ,Pressure_Node_29 NUMERIC(6,3)
+  ,Pressure_Node_30 NUMERIC(6,3)
+  ,Pressure_Node_31 NUMERIC(6,3)
+  ,Pressure_Node_32 NUMERIC(6,3)
+);
